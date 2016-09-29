6200b4631bb62bd79d4d90e17b4f52ca7d00db05
Ryan Vo <ryanvo@seas.upenn.edu>
2016-09-29 03:35:41 -0400

    add blockingqueue

--- src/edu/upenn/cis/cis455/webserver/MyBlockingQueue.java
+++ src/edu/upenn/cis/cis455/webserver/MyBlockingQueue.java
@@ -3,27 +3,42 @@ package edu.upenn.cis.cis455.webserver;
 import java.util.ArrayDeque;
 import java.util.Queue;
 
-/**
- * Created by ryan on 9/29/16.
- */
-public class MyBlockingQueue<T> {
 
-        private Queue<T> queue = new ArrayDeque<T>();
-        private int size;
+public class MyBlockingQueue<E> {
 
-        public MyBlockingQueue(int size) {
-            this.size = size;
+    private Queue<E> queue = new ArrayDeque<E>();
+    private int size;
+
+    public MyBlockingQueue(int size) {
+        this.size = size;
+    }
+
+    public synchronized void put(E request) {
+
+        try{
+            while (queue.size() == size)
+                wait();
+        } catch (InterruptedException e) {
+            //TODO handle?
         }
 
-        public synchronized void put(T request) {
+    }
 
-            try{
-                while(queue.size() == size)
-                    wait();
-            } catch (InterruptedException e) {
-                //TODO handle?
-            }
+    public synchronized E take() {
+
+        try {
+            while (queue.isEmpty())
+                wait();
+        } catch (InterruptedException e) {
+
+            //TODO handle?
 
         }
+
+        E request = queue.remove();
+        notify();
+        return request;
+    }
+}
 
 }
